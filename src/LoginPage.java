25d334cb111c91c7042bf11fdbb30c04aecf32f8
Mostafa Nasser <mostafanasser@Mostafas-MacBook-Air.local>
2025-06-01 04:06:15 +0300

    Added login and signup pages

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,7 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginPage {
     private JPanel panel1;
     private JTextField textField1;
     private JPasswordField passwordField1;
+    private JButton LOGINButton;
+    private JFrame frame;
+    public LoginPage(){
+        frame = new JFrame("Login Page");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(600, 400));
+        frame.setResizable(false);
+        // Add Panels
+        frame.add(panel1);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
---
+++ src/SignupPage.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SignupPage {
+    private JTextField textField1;
+    private JPanel panel1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JButton SIGNUPButton;
+}
